7387d2dc7b02821643119b1dcd8572776921e820
franciel <francielvictor@gmail.com>
2021-06-15 12:43:14 -0300

    cricao do arquivo hoje.php

---
+++ hoje.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title><?php echo "Dia ".date("d/m/Y");?></title>
+		<meta charset="utf-8"/>
+	</head>
+	<body>
+		<h1>Estamos em <?php echo date('Y'); ?> e hoje é dia <?php echo date("d/m"); ?></h1>
+		<p>Esta página foi gerada às <?php echo date("H");?> Horas e <?php echo date('i');?> minutos</p>
+	</body>
+</html>
